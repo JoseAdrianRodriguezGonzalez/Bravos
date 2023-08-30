6a65be81d097cf3de015f523b9b9be3011e9b914
JoseAdrianRodriguezGonzalez <kerpador@gmail.com>
2023-08-30 09:07:52 -0600

    Editar php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-require_once "/Principal_es/index.html"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        require_once "/Principal_es/index.html"
+    ?>
+</body>
+</html>
