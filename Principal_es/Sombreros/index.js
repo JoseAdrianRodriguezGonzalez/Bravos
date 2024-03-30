e89c91c45df3b95111d0673d48d458b6a7f60f60
Jose Adrian <kerpador@gmail.com>
2024-03-30 11:17:43 -0600

    añadir la tienda

---
+++ Principal_es/Sombreros/index.js
@@ -0,0 +1,38 @@
+const express =require("express");
+const database=require("mysql")
+const excel=require("xlsx");
+const app =express();
+const port=3000;
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended:true,
+    })
+);
+const db= database.createConnection({
+    host:"localhost",
+    user:"root",
+    password:'',
+    database:"bravos"
+})
+let data=excel.readFile('./Sombreros.xlsx');
+let worksheet =data.Sheets[data.SheetNames[1]];
+let range=excel.utils.decode_range(worksheet["!ref"])
+for (let row = range.s.r + 1; row <= range.e.r; row++) {
+    let datacol = [];
+    for (let col = range.s.c; col <= range.e.c; col++) {
+        let cell = worksheet[excel.utils.encode_cell({ r: row, c: col })];
+        datacol.push(cell ? cell.v : -1);
+    }
+    console.log(datacol);
+   // let sql = "INSERT INTO `tejidos` (`Tipo de sombrero`, `Color`, `Nombre`, `Medidas`, `Calidad`, `Precio`, `Unidad`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+  //let sql = "INSERT INTO `botas` (`Material`, `Forma`, `Color`) VALUES (?, ?, ? )";
+    let sql ="INSERT INTO `texana` (`Tipo de sombrero`, `Color`, `Tamaño`, `Calidad`, `Precio`, `Unidad`) VALUES (?, ?, ?, ?, ?, ?)"
+  db.query(sql, datacol, (err, results, fields) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        console.log("User ID:" + results.insertId);
+    });
+}
+db.end();
---
+++ Principal_es/Sombreros/script.js
@@ -0,0 +1,113 @@
+let mx =document.getElementById('mexico');
+let usa=document.getElementById('usa');
+let correo=document.getElementById('correo');
+let cosas=document.getElementById('a');
+let contactos=document.getElementById('contactos-btn');
+
+let izquierda=document.getElementById('izq');
+let derecha=document.getElementById('der');
+let grupo=document.getElementById('gr');
+let animacion=document.getElementById('animacion');
+let arreglo=[].slice.call(grupo.children);
+let Mai=document.getElementById('Mai');
+console.log(arreglo instanceof Array);
+window.sr=ScrollReveal();
+
+animacion.load();
+let a=false;
+mx.addEventListener('click',()=>{
+    location.href="/Principal_es/index.html";
+})
+usa.addEventListener('click',()=>{
+    location.href="/Principal_en/index.html";
+})
+correo.addEventListener('click',()=>{
+    location.href="mailto:bravosboots.hats@gmail.com"
+})
+Mai.addEventListener('click',()=>{
+    location.href="mailto:bravosboots.hats@gmail.com"
+})
+contactos.addEventListener('click',()=>{
+    
+    if(a==false){
+        cosas.classList.add("abierto");
+        cosas.classList.replace("todos-cont","abierto");
+        a=true;
+    }else{
+        cosas.classList.remove("abierto");
+        cosas.classList.add("todos-cont")
+        a=false;
+        
+    }
+});
+
+derecha.addEventListener('click',()=>{
+    ultimo=arreglo.pop();
+
+    arreglo.unshift(ultimo);
+    ultimo.classList.add('animacion');
+
+    setTimeout(() => {
+        grupo.insertAdjacentElement('afterbegin',ultimo);        
+        if(ultimo.classList.contains('animacion')){
+            ultimo.classList.remove('animacion');
+             
+        }
+    }, 500);
+
+})
+izquierda.addEventListener('click',()=>{
+    primero=arreglo.shift();
+    arreglo.push(primero);
+    primero.classList.add('animacion');
+    setTimeout(() => {
+
+        grupo.insertAdjacentElement('afterbegin',primero);        
+        if(primero.classList.contains('animacion')){
+            primero.classList.remove('animacion');
+           
+        }
+    }, 500);
+})
+/***Manejo de JSON */
+let url = "/xampp/htdocs/bravo/Principal_es/perfil/index.php";
+let request = new XMLHttpRequest();
+
+request.open('POST', '/Principal_es/perfil/Variables.php', true);
+
+request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+        // Success
+        let parsed_response = JSON.parse(this.response.trim());
+        console.log(parsed_response);
+        ManejarJSON(parsed_response);
+    } else {
+        // Error
+        console.log(this.response);
+    }
+};
+request.onerror = function() {
+    console.log('Connection error!');
+};
+request.send();
+/**Manejo de los AJAX */
+const EncontrarDato=(Arreglo)=>{
+    for(key in Arreglo) {
+        if(Arreglo.hasOwnProperty(key)) {
+            let value = Arreglo[key];
+            //do something with value;
+            return value["Nombre"]
+        }
+    }
+}
+const ManejarJSON=(JSONPHP)=>{
+    let texto=EncontrarDato(JSONPHP);
+    let ini=document.getElementById('log');
+    let link=document.getElementById('padrea');
+    const ini2=document.createElement('span');
+    ini2.classList.add('ini2','log');
+    ini2.textContent=texto;
+    ini.append(ini2)
+    link.replaceChild(ini2,ini);
+    link.setAttribute("href","/Principal_es/perfil/index.html");
+}
