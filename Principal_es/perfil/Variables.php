da78dd202290a84bac88f5bd4bbb958a00a72e6f
Jose Adrian <kerpador@gmail.com>
2024-07-13 12:06:55 -0600

    b

--- Principal_es/perfil/Variables.php
+++ Principal_es/perfil/Variables.php
@@ -1,6 +1,6 @@
 <?php
-        echo "a";
     require "/../Inicio/sesion/inicio.php";//Llama a la "libreria" de inicio
+    echo "a";
     
     require "/../Inicio/sesion/sesion.php";//Llama a la libreria de sesio
     $userSession = new SesionUsser();//se crea un nuevo objeto de la clase SesionUsser
