562593b2a2bdc41cb08af551b7f2d4049facd6f4
Jose Adrian <kerpador@gmail.com>
2024-07-13 12:53:14 -0600

    a

--- Principal_es/script.js
+++ Principal_es/script.js
@@ -80,7 +80,7 @@ request.onload = function() {
         // Success
         
         let parsed_response = JSON.parse(this.response.trim());
-        if(!parsed_response.length()){console.log(2)}
+        if(parsed_response.length()==0){console.log(2)}
         console.log(parsed_response.length());
         ManejarJSON(parsed_response);
     } else {
