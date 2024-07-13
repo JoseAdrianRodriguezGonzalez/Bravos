6919d7926b380fafdadcfcfee784e65a8f4e6f9d
Jose Adrian <kerpador@gmail.com>
2024-07-12 19:46:11 -0600

    a

--- Principal_es/perfil/Variables.php
+++ Principal_es/perfil/Variables.php
@@ -1,6 +1,5 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+
     require "./../Inicio/sesion/inicio.php";//Llama a la "libreria" de inicio
     
     require "./../Inicio/sesion/sesion.php";//Llama a la libreria de sesio
