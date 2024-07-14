eba2eab8679a410acab7bffaf2c792eee3492bad
Jose Adrian <kerpador@gmail.com>
2024-07-14 13:38:35 -0600

    a

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -1,14 +1,14 @@
 <?php
     function conectar(){
-        $server='localhost';
+      /*  $server='localhost';
         $user='u350875428_bravo';
         $pass='Bravoshats23456%';
         $dbname='u350875428_bravos';
-       /* $server='localhost';
+       */ $server='localhost';
         $user='root';
         $pass='';
         $dbname='bravos';
-        */$conexion= new mysqli($server,$user,$pass,$dbname);
+        $conexion= new mysqli($server,$user,$pass,$dbname);
         if($conexion -> connect_error){
             die("Conexion fallidad".$conexion->connect_error);
         }else {
