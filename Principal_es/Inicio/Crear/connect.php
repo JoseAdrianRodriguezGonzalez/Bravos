aef29f18d85f917ab4a3357d30811b3f2faa7578
Jose Adrian <kerpador@gmail.com>
2024-07-15 11:17:21 -0600

    the specific direction

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -1,5 +1,5 @@
 <?php
-    require('../../../vendor/autoload.php');
+    require('/../../../../vendor/autoload.php');
     $dotenv=Dotenv\Dotenv::createImmutable('../../../');
     $dotenv->load();
     
