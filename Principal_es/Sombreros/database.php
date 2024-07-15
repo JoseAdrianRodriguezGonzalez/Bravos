7b9b6b92fd0feb8e4a0a89c2242a7e83458a1379
Jose Adrian <kerpador@gmail.com>
2024-07-15 14:27:44 -0600

    por fa

--- Principal_es/Sombreros/database.php
+++ Principal_es/Sombreros/database.php
@@ -3,10 +3,10 @@
     require dirname(__DIR__,2)."/vendor/DotEnv.php";
     use DevCoder\DotEnv;
     (new DotEnv(__DIR__ . '/../../../.env'))->load();
+    
+    
+    echo $_ENV('servidor');
     echo"si";
-    
-    
-    echo getenv('servidor');
     function peticion($instruccion){
     $query1=mysqli_query(conectar($_ENV['server'],$_ENV['db_user'],$_ENV['db_pass'],$_ENV['db_name']),$instruccion); //envia peticion
     
