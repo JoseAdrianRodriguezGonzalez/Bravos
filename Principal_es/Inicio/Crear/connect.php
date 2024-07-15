1de00dbdc9a9e389cc654f3d725403da37d09458
Jose Adrian <kerpador@gmail.com>
2024-07-15 15:15:08 -0600

    2podre

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -4,9 +4,8 @@
       (new DotEnv(__DIR__ . '/../../../../.env'))->load();
    // $dotenv=Dotenv\Dotenv::createImmutable('../../../');
    // $dotenv->load();
-    echo $_ENV['server'];
     function conectar($server,$user,$pass,$dbname){  
-        $conexion= mysqli_connect($server,$user,$pass,$dbname);
+        $conexion= mysqli_connect($_ENV['server'],$_ENV['db_user'],$_ENV['db_pass'],$_ENV['db_name']);
         if($conexion -> connect_error){
             die("Conexion fallidad".$conexion->connect_error);
         }else {
