b5ca761d453b9db1fb6f0cb4c67d265fed909662
Jose Adrian <kerpador@gmail.com>
2024-07-15 14:57:38 -0600

    a

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -4,7 +4,7 @@
    // $dotenv->load();
     
     function conectar($server,$user,$pass,$dbname){  
-        $conexion= new mysqli($server,$user,$pass,$dbname);
+        $conexion= mysqli_connect($server,$user,$pass,$dbname);
         echo "si";
         
         if($conexion -> connect_error){
