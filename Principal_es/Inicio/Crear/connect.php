8e024c8483d7fd753fa8586fa7fe0de27f00c0c5
Jose Adrian <kerpador@gmail.com>
2024-07-15 15:12:50 -0600

    quizas

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -1,5 +1,7 @@
 <?php
-    
+      require __DIR__."/../../../vendor/DotEnv.php";
+      use DevCoder\DotEnv;
+      (new DotEnv(__DIR__ . '/../../../../.env'))->load();
    // $dotenv=Dotenv\Dotenv::createImmutable('../../../');
    // $dotenv->load();
     
