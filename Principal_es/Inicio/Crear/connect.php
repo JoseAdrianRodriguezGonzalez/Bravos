dbbbb64ff09374b401675e558f82082ebdd172fb
Jose Adrian <kerpador@gmail.com>
2024-07-15 15:25:31 -0600

    porque nos ale

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -1,15 +1,17 @@
 <?php
-      require __DIR__."/../../../vendor/DotEnv.php";
-      use DevCoder\DotEnv;
-      (new DotEnv(__DIR__ . '/../../../../.env'))->load();
+    
    // $dotenv=Dotenv\Dotenv::createImmutable('../../../');
    // $dotenv->load();
     
     function conectar($server,$user,$pass,$dbname){  
+
         $conexion= mysqli_connect($server,$user,$pass,$dbname);
+        
         if($conexion -> connect_error){
+            echo "no";
             die("Conexion fallidad".$conexion->connect_error);
         }else {
+            echo'si';
             return $conexion;
         }
        
