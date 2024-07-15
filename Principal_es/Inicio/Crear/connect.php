3d133a559fe64ee6318472b4ab8aa7cf60002aef
Jose Adrian <kerpador@gmail.com>
2024-07-15 11:16:20 -0600

    new files

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -1,14 +1,19 @@
 <?php
+    require('../../../vendor/autoload.php');
+    $dotenv=Dotenv\Dotenv::createImmutable('../../../');
+    $dotenv->load();
+    
     function conectar(){
-       $server='localhost';
-        $user='u350875428_bravo';
-        $pass='Bravoshats23456%';
-        $dbname='u350875428_bravos';
+       $server=$_ENV['server'];//look
+        $user=$_ENV['db_user'];//look
+        $pass=$_ENV['db_pass'];//look
+        $dbname=$_ENV['db_name'];//look
        /* $server='localhost';
         $user='root';
         $pass='';
         $dbname='bravos';
-        */$conexion= new mysqli($server,$user,$pass,$dbname);
+        */
+        $conexion= new mysqli($server,$user,$pass,$dbname);
         if($conexion -> connect_error){
             die("Conexion fallidad".$conexion->connect_error);
         }else {
