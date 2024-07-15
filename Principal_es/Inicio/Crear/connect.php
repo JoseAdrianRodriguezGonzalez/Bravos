24bd1a71ac8806739fde20582045cd545d771dea
Jose Adrian <kerpador@gmail.com>
2024-07-15 11:26:50 -0600

    a

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -1,18 +1,18 @@
 <?php
     require('../../../vendor/autoload.php');
-    $dotenv=Dotenv\Dotenv::createImmutable('../../../../../');
+    $dotenv=Dotenv\Dotenv::createImmutable('../../../');
     $dotenv->load();
     
     function conectar(){
-       $server=$_ENV['server'];//look
-        $user=$_ENV['db_user'];//look
-        $pass=$_ENV['db_pass'];//look
-        $dbname=$_ENV['db_name'];//look
-       /* $server='localhost';
-        $user='root';
-        $pass='';
-        $dbname='bravos';
-        */
+      // $server=$_ENV['server'];//look
+       // $user=$_ENV['db_user'];//look
+       // $pass=$_ENV['db_pass'];//look
+       // $dbname=$_ENV['db_name'];//look
+        $server=$_ENV['servidor'];
+        $user=$_ENV['usuario'];
+        $pass=$_ENV['contra'];
+        $dbname=$_ENV['nombre'];
+        
         $conexion= new mysqli($server,$user,$pass,$dbname);
         if($conexion -> connect_error){
             die("Conexion fallidad".$conexion->connect_error);
