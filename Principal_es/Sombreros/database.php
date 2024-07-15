ddebc99bc2932b7e15dcaf000662a517881d25d3
Jose Adrian <kerpador@gmail.com>
2024-07-15 15:17:25 -0600

    wfe

--- Principal_es/Sombreros/database.php
+++ Principal_es/Sombreros/database.php
@@ -2,12 +2,16 @@
     require "./../Inicio/Crear/connect.php";
     require __DIR__."/../../vendor/DotEnv.php";
     use DevCoder\DotEnv;
+
+use function PHPSTORM_META\type;
+
     (new DotEnv(__DIR__ . '/../../../.env'))->load();
     function peticion($instruccion){
         $server=$_ENV['server'];
         $user=$_ENV['db_user'];
         $pass=$_ENV['db_pass'];
         $dbname=$_ENV['db_name'];
+        echo gettype($server);
         $query1=mysqli_query(conectar($server,$user,$pass,$dbname),$instruccion); //envia peticion
             
     
