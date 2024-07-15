dec4df18fee1890b5c4aeee8ccd31e9435f15d98
Jose Adrian <kerpador@gmail.com>
2024-07-15 15:04:57 -0600

    ergrf

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -5,9 +5,9 @@
     
     function conectar($server,$user,$pass,$dbname){  
 
-        $conexion= mysqli_connect($server,$user,$pass,$dbname);
-        error_reporting(E_ALL); 
         ini_set('display_errors', 1);
+        error_reporting(E_ALL); 
+        $conexion= mysqli_connect($server,$user,$pass,$dbname);
         if($conexion -> connect_error){
             echo "no";
             die("Conexion fallidad".$conexion->connect_error);
