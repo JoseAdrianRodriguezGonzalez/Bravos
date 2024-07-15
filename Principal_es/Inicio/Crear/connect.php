de39e4130908f3cbc76f4ce7d4a93dd6ffc522b6
Jose Adrian <kerpador@gmail.com>
2024-07-15 13:33:46 -0600

    A

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -2,7 +2,7 @@
     
    // $dotenv=Dotenv\Dotenv::createImmutable('../../../');
    // $dotenv->load();
-    
+    echo "b";
     function conectar($server,$user,$pass,$dbname){  
         $conexion= new mysqli($server,$user,$pass,$dbname);
         if($conexion -> connect_error){
