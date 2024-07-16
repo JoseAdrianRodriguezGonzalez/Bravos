88189ba8841a8f87615d7b1fccf14786dd5cf2a1
Jose Adrian <kerpador@gmail.com>
2024-07-16 12:28:07 -0600

    prueba de nuevo

---
+++ Principal_es/Inicio/sesion/connect.php
@@ -0,0 +1,21 @@
+<?php
+    
+   // $dotenv=Dotenv\Dotenv::createImmutable('../../../');
+   // $dotenv->load();
+   require __DIR__."/../../../vendor/DotEnv.php";
+   use DevCoder\DotEnv;
+   (new DotEnv(__DIR__ . '/../../../../.env'))->load();
+   
+    function conectar(){  
+
+        $conexion= mysqli_connect(getenv('server'),getenv('db_user'),getenv('db_pass'),getenv('db_name'));
+        
+        if($conexion -> connect_error){
+            die("Conexion fallidad".$conexion->connect_error);
+        }else {
+            
+            return $conexion;
+        }
+       
+    }
+?>
--- Principal_es/Inicio/sesion/inicio.php
+++ Principal_es/Inicio/sesion/inicio.php
@@ -1,6 +1,6 @@
 <?php
     
-    require(__DIR__."../../Crear/connect.php");
+    require(__DIR__."/connect.php");
     
    //Clase usuario
     class user {
