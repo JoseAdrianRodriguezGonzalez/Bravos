e741aab2e96feb6abdaa84d8e3e87eb7e43d207a
Jose Adrian <kerpador@gmail.com>
2024-07-16 11:08:49 -0600

    las rutassssssssssssssssss

--- Principal_es/Inicio/sesion/GoogleOAuth.php
+++ Principal_es/Inicio/sesion/GoogleOAuth.php
@@ -1,8 +1,8 @@
 <?php
     require "sesion.php";
     require "inicio.php";
-    require (__DIR__."/../../../vendor/DotEnv.php");
     echo"si";
+    require __DIR__."/../../../vendor/DotEnv.php";
     use DevCoder\DotEnv;
     (new DotEnv(__DIR__ . '/../../../../.env'))->load();
     
