e6a1ec5a947685093177b3806a60b66cc34d95e7
Jose Adrian <kerpador@gmail.com>
2024-07-16 10:50:24 -0600

    re

--- Principal_es/Inicio/sesion/GoogleOAuth.php
+++ Principal_es/Inicio/sesion/GoogleOAuth.php
@@ -1,12 +1,13 @@
 <?php
     require "sesion.php";
+    require "inicio.php";
     require __DIR__."/../../../vendor/DotEnv.php";
     use DevCoder\DotEnv;
     (new DotEnv(__DIR__ . '/../../../../.env'))->load();
     
     $NewUserSession= new SesionUsser();
-    echo"si";
     $NewUser=new user();
+    echo"si";
     $google_OAuth_ID=getenv('ID-client-google');
     $google_OAuth_Secret=getenv('Secret-Client-Google');
     $google_OAuth_redirect=getenv('redirect-api');
