37ce78467b0a8deb859c14fa46689638955d1437
Jose Adrian <kerpador@gmail.com>
2024-07-16 11:00:13 -0600

    a

--- Principal_es/Inicio/sesion/GoogleOAuth.php
+++ Principal_es/Inicio/sesion/GoogleOAuth.php
@@ -1,6 +1,6 @@
 <?php
     require __DIR__."/sesion.php";
-    require __DIR__."inicio.php";
+    require __DIR__."/inicio.php";
     require __DIR__."/../../../vendor/DotEnv.php";
     use DevCoder\DotEnv;
     (new DotEnv(__DIR__ . '/../../../../.env'))->load();
