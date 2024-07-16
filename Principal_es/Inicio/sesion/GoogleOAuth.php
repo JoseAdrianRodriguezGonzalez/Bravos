e1876808e511d615e6652a4709de17237e0f339a
Jose Adrian <kerpador@gmail.com>
2024-07-16 11:47:19 -0600

    sale

--- Principal_es/Inicio/sesion/GoogleOAuth.php
+++ Principal_es/Inicio/sesion/GoogleOAuth.php
@@ -5,12 +5,12 @@
     require __DIR__ ."/sesion.php";
     //$userSession=new SesionUsser();
     //// $NewUser=new user();
-    echo $_ENV['ID-client-google'];  
-    $google_OAuth_ID=getenv('ID-client-google');
+    echo "hola";  
+    /*$google_OAuth_ID=getenv('ID-client-google');
     $google_OAuth_Secret=getenv('Secret-Client-Google');
     $google_OAuth_redirect=getenv('redirect-api');
     $google_OAuth_version='v3';
-
+*/
 /*
 
     if (isset($_GET['code']) && !empty($_GET['code'])) {
