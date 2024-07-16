4a13512f8d8d203b802e179fa4091d2241827a14
Jose Adrian <kerpador@gmail.com>
2024-07-16 13:06:51 -0600

    integration code

--- Principal_es/Inicio/sesion/GoogleOAuth.php
+++ Principal_es/Inicio/sesion/GoogleOAuth.php
@@ -45,4 +45,41 @@
         header('Location: https://accounts.google.com/o/oauth2/auth?' . http_build_query($params));
         exit;
     }
+    if (isset($response['access_token']) && !empty($response['access_token'])) {
+        // Execute cURL request to retrieve the user info associated with the Google account
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/oauth2/' . $google_OAuth_version . '/userinfo');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $response['access_token']]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $profile = json_decode($response, true);
+        // Make sure the profile data exists
+        if (isset($profile['email'])) {
+            $google_name_parts = [];
+            $google_name_parts[] = isset($profile['given_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['given_name']) : '';
+            $google_name_parts[] = isset($profile['family_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['family_name']) : '';
+            // Code goes here...
+            
+            session_regenerate_id();
+            $_SESSION['google_loggedin'] = TRUE;
+            $_SESSION['google_email'] = $profile['email'];
+            $_SESSION['google_name'] = implode(' ', $google_name_parts);
+            echo $profile;
+        } else {
+            exit('Could not retrieve profile information! Please try again later!');
+        }
+    } else {
+        exit('Invalid access token! Please try again later!');
+        $params = [
+            'response_type' => 'code',
+            'client_id' => $google_OAuth_ID,
+            'redirect_uri' => $google_OAuth_redirect,
+            'scope' => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+            'access_type' => 'offline',
+            'prompt' => 'consent'
+        ];
+        header('Location: https://accounts.google.com/o/oauth2/auth?' . http_build_query($params));
+        exit;
+    }
 ?>
