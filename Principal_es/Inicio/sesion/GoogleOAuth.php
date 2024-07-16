9d1bd0b66e87a06272b772b2cba7c8002a566eca
Jose Adrian <kerpador@gmail.com>
2024-07-16 13:31:35 -0600

    fe

--- Principal_es/Inicio/sesion/GoogleOAuth.php
+++ Principal_es/Inicio/sesion/GoogleOAuth.php
@@ -65,6 +65,7 @@
             $_SESSION['google_loggedin'] = TRUE;
             $_SESSION['google_email'] = $profile['email'];
             $_SESSION['google_name'] = implode(' ', $google_name_parts);
+            echo  $_SESSION['google_email'];
         } else {
             exit('Could not retrieve profile information! Please try again later!');
         }
