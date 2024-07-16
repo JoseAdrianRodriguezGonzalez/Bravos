7fb07d2cc75d0c23cb050111b4357cb2758b48d3
Jose Adrian <kerpador@gmail.com>
2024-07-16 12:03:37 -0600

    hola

--- Principal_es/Inicio/sesion/datos.php
+++ Principal_es/Inicio/sesion/datos.php
@@ -2,5 +2,9 @@
     require __DIR__."/../../../vendor/DotEnv.php";
     use DevCoder\DotEnv;
     (new DotEnv(__DIR__ . '/../../../../.env'))->load();
-    echo "hola";  
+    $google_OAuth_ID=getenv('ID-client-google');
+    $google_OAuth_Secret=getenv('Secret-Client-Google');
+    $google_OAuth_redirect=getenv('redirect-api');
+    $google_OAuth_version='v3';
+    echo $google_OAuth_ID;
 ?>
