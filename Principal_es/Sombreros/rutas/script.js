f0ab789cd80f890a482cb6027fee5b75e53b3128
Jose Adrian <kerpador@gmail.com>
2024-12-19 13:29:39 -0600

    mobile

--- Principal_es/Sombreros/rutas/script.js
+++ Principal_es/Sombreros/rutas/script.js
@@ -94,28 +94,53 @@ var addZoom = target => {
       // (B1) CALCULATE ZOOM RATIO
       let ratio = img.naturalHeight / img.naturalWidth,
           percentage = ratio * 100 + "%";
-   
+        
+        if(window.innerWidth<=800){
+            container.click=e=>{
+                container.onmousemove = e => {
+                    let rect = e.target.getBoundingClientRect(),
+                        xPos = e.clientX - rect.left,
+                        yPos = e.clientY - rect.top,
+                        xPercent = xPos / (container.clientWidth / 100) + "%",
+                        yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
+        
+                    Object.assign(container.style, {
+                    backgroundPosition: xPercent + " " + yPercent,
+                    backgroundSize: img.naturalWidth + "px"
+                    });
+                };
+        
+                // (B3) RESET ZOOM ON MOUSE LEAVE
+                container.onmouseleave = e => {
+                    Object.assign(container.style, {
+                    backgroundPosition: "center",
+                    backgroundSize: "cover"
+                    });
+                } 
+            }
+        }else{
       // (B2) ATTACH ZOOM ON MOUSE MOVE
-      container.onmousemove = e => {
-        let rect = e.target.getBoundingClientRect(),
-            xPos = e.clientX - rect.left,
-            yPos = e.clientY - rect.top,
-            xPercent = xPos / (container.clientWidth / 100) + "%",
-            yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
-   
-        Object.assign(container.style, {
-          backgroundPosition: xPercent + " " + yPercent,
-          backgroundSize: img.naturalWidth + "px"
-        });
-      };
-   
-      // (B3) RESET ZOOM ON MOUSE LEAVE
-      container.onmouseleave = e => {
-        Object.assign(container.style, {
-          backgroundPosition: "center",
-          backgroundSize: "cover"
-        });
-      };
+        container.onmousemove = e => {
+            let rect = e.target.getBoundingClientRect(),
+                xPos = e.clientX - rect.left,
+                yPos = e.clientY - rect.top,
+                xPercent = xPos / (container.clientWidth / 100) + "%",
+                yPercent = yPos / ((container.clientWidth * ratio) / 100) + "%";
+
+            Object.assign(container.style, {
+            backgroundPosition: xPercent + " " + yPercent,
+            backgroundSize: img.naturalWidth + "px"
+            });
+        };
+
+        // (B3) RESET ZOOM ON MOUSE LEAVE
+        container.onmouseleave = e => {
+            Object.assign(container.style, {
+            backgroundPosition: "center",
+            backgroundSize: "cover"
+            });
+        };
+        }
     }
   };
    
