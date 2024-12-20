bbb5924334fbc4986044b63262e5af9128f5ba3d
Jose Adrian <kerpador@gmail.com>
2024-12-20 15:53:11 -0600

    adding a button to buy products

--- Principal_es/Inicio/Crear/connect.php
+++ Principal_es/Inicio/Crear/connect.php
@@ -4,8 +4,16 @@
    // $dotenv->load();
    require __DIR__."/../../../vendor/DotEnv.php";
    use DevCoder\DotEnv;
-   (new DotEnv(__DIR__ . '/../../../../.env'))->load();
    
+   $isLocal = in_array($_SERVER['HTTP_HOST'], ['localhost:3000', '127.0.0.1', '::1']);
+   if ($isLocal) {
+    (new DotEnv(__DIR__ . '/../../../.env.local'))->load(); // Local environment file
+    
+} else {
+    (new DotEnv(__DIR__ . '/../../../../.env'))->load(); // Production (Hostinger) environment file
+    echo "I am not local";
+    }
+
     function conectar(){  
 
         $conexion= mysqli_connect(getenv('server'),getenv('db_user'),getenv('db_pass'),getenv('db_name'));
